1f7d194693e72f8c5d266cbe4573e8e5f207b83e
Lucas Lopes <lucascaldaslopes15@gmail.com>
2022-04-06 20:43:21 -0300

    Styled header and feedback

---
+++ src/components/Header/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.text};
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const AvatarImageContainer = styled.img`
+  display: flex;
+  margin-left: 30px;
+  margin-top: 30px;
+  border-radius: 50px;
+`;
+
+export const AvatarInfosContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
